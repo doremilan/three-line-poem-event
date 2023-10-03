a08e9689ee9a076aa06f6589122b07682a48c49c
Milan Lee <100390926+doremilan@users.noreply.github.com>
2023-10-03 21:57:10 +0900

    fix: cors 설정 추가

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-//const cors = require('cors');
+const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const passport = require('./passport');
@@ -9,7 +9,7 @@ const app = express();
 
 passport(app);
 
-// app.use(cors());
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
